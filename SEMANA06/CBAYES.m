80a392e90f3aa325f7ecd806825f3251b518c0de
felipehideki <feehide@gmail.com>
2021-01-07 18:19:07 -0300

    Update CBAYES.m

--- SEMANA06/CBAYES.m
+++ SEMANA06/CBAYES.m
@@ -9,15 +9,28 @@ function [probabilidades, classificacao] = CBAYES(u,sigma,prior,x)
     %           classificacao = classe com maior P(w|x)
     if size(u)~=size(sigma)
         error('Quantidade de médias diferente da quantidade de matrizes de covariância!');
-    else
-        L = numel(u);
     end
-    for i=1:L
-        if size(u{i})~=size(sigma{i})
-            error('Dimensão de médias diferente de dimensão das matrizes de covariância!');
+    L = numel(u);
+    px = 0;
+    aux = 0;
+    i = 1;
+    while i <= L
+        if ~aux
+            if size(u{i})~=size(sigma{i})
+                error('Dimensão de médias diferente de dimensão das matrizes de covariância!');
+            end
+            pxw(i) = (1/sqrt(((2*pi)^L)*det(sigma{i})))*exp(-0.5*(x-u{i})'*inv(sigma{i})*(x-u{i}));
+            px = px + pxw(i)*prior(i); % Marginalização P(x) = sum_classe(P(x|classe)P(classe))
+            if i==L
+                aux = 1;
+                i = 1;
+            else
+                i = i+1;
+            end
         else
-            probabilidades(i) = prior(i)*(1/sqrt(((2*pi)^L)*det(sigma{i})))*exp(-0.5*(x-u{i})'*inv(sigma{i})*(x-u{i}));
+            probabilidades(i) = pxw(i)*prior(i)/px;
+            i = i+1;
         end
-    end
+    end   
     classificacao = find(probabilidades==max(probabilidades));
 end
