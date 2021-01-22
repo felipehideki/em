d43881f7635dccaa4a97d705fb51d5a2d0cda48f
felipehideki <feehide@gmail.com>
2021-01-22 18:18:00 -0300

    Update LS.m

--- SEMANA08/LS.m
+++ SEMANA08/LS.m
@@ -22,14 +22,29 @@ function [w] = LS(xClasse1,xClasse2,alpha)
     w = inv((y'*y)+alpha*ones(size(y,2)))*(y'*classes);
     
     % Plot
-    c1 = plot(xClasse1(:,1),xClasse1(:,2),'.b'); 
-    hold on; 
-    c2 = plot(xClasse2(:,1),xClasse2(:,2),'.r');
-    hold on;
-    dominio = linspace(min(y(:,1)), max(y(:,1)));
-    imagem = -(w(3)+w(1)*dominio)/w(2);
-    ls = plot(dominio,imagem,'-black');
-    title(['\alpha = ', num2str(alpha,'%.2f')]);
-    legend([c1,c2,ls], {'classe 1','classe 2','LS'});
-    toc;
+    switch size(y,2)
+        case 3 % L+1=3 (L = 2 dimensões)
+            c1 = plot3(xClasse1(:,1),xClasse1(:,2),'.b'); 
+            hold on; 
+            c2 = plot3(xClasse2(:,1),xClasse2(:,2),'.r');
+            hold on;
+            dominio = linspace(min(y(:,1)), max(y(:,1)));
+            imagem = -(w(3)+w(1)*dominio)/w(2); % plano w1x+w2y+w3=0
+            ls = plot(dominio,imagem,'-black');
+            title(['\alpha = ', num2str(alpha,'%.2f')]);
+            legend([c1,c2,ls], {'classe 1','classe 2','LS'});
+            toc;
+        case 4 % L+1=4 (L = 3 dimensões)
+            c1 = plot3(xClasse1(:,1),xClasse1(:,2),xClasse1(:,3),'.b'); 
+            hold on; 
+            c2 = plot3(xClasse2(:,1),xClasse2(:,2),xClasse2(:,3),'.r');
+            hold on;
+            [x y] = meshgrid(min(y(:,1)):max(y(:,1)),min(y(:,2)):max(y(:,2)));
+            z = -((w(1)*x)+(w(2)*y)+w(4))/w(3); % plano w1x+w2y+w3z+w4=0
+            ls = surf(x,y,z,ones(size(x)));
+            ls.EdgeColor = 'none';
+            title(['\alpha = ', num2str(alpha,'%.2f')]);
+            legend([c1,c2,ls], {'classe 1','classe 2','LS'});
+            toc;
+    end
 end
