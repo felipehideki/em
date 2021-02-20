b01bcb375db75a520567d1ccefc4f4c944e3754c
felipehideki <feehide@gmail.com>
2021-02-19 23:43:12 -0300

    Update TRABALHOFINAL_GRUPO1.m

--- TRABALHO_FINAL/TRABALHOFINAL_GRUPO1.m
+++ TRABALHO_FINAL/TRABALHOFINAL_GRUPO1.m
@@ -1,5 +1,5 @@
 % % TRABALHO FINAL DE ENGENHARIA MÉDICA 2S/2020
-% % GRUPO 1
+% % GRUPO 1: FELIPE HIDEKI HATANO & PEDRO ROCHA SANTOS
 
 % % CLASSIFICAÇÃO DE INDIVÍDUOS SAUDÁVEIS/DOENTES DE UM BANCO DE DADOS DE
 % % AUSCULTAÇÕES TORÁXICAS
@@ -7,7 +7,7 @@
 
 
 % % EXTRAINDO INÍCIO E FINAL DOS CICLOS RESPIRATÓRIOS EM CADA GRAVAÇÃO
-pathname = 'C:\Users\$USER$\Desktop\EngenhariaMedica\TRABALHOFINAL\Respiratory_Sound_Database\Respiratory_Sound_Database\audio_and_txt_files';
+pathname = 'C:\Users\f8\Desktop\EngenhariaMedica\TRABALHOFINAL\Respiratory_Sound_Database\Respiratory_Sound_Database\audio_and_txt_files';
 
 S = dir(fullfile(pathname,'*.txt'));
 outtxt = cell(size(S));
@@ -23,24 +23,25 @@ clear filename filepath k S w
 
 S = dir(fullfile(pathname,'*.wav'));
 ciclos = cell(size(S));
+fs = zeros(size(S));
 w = waitbar(0,'Importando arquivos .wav');
 for k = 1:numel(S)
     waitbar(k/numel(S));
     filename = S(k).name;
     filepath = fullfile(pathname,filename);
-    [dados,fs] = audioread(filepath);
+    [dados,fs(k)] = audioread(filepath);
     for i = 1:size(outtxt{k},1)
-        if round(outtxt{k}(i,1)*fs)
-            ciclos{k,i} = dados((floor(outtxt{k}(i,1)*fs)):(ceil(outtxt{k}(i,2)*fs)));
+        if floor(outtxt{k}(i,1)*fs(k))
+            ciclos{k,i} = dados((floor(outtxt{k}(i,1)*fs(k))):(ceil(outtxt{k}(i,2)*fs(k))));
         else %caso o primeiro índice do ciclo seja 0
-            ciclos{k,i} = dados(1:(ceil(outtxt{k}(i,2)*fs)));
+            ciclos{k,i} = dados(1:(ceil(outtxt{k}(i,2)*fs(k))));
         end
     end
 end
 delete(w);
-clear dados filename filepath fs i k w outtxt
+clear dados filename filepath i k w outtxt
 
-T = readtable('C:\Users\$USER$\Desktop\EngenhariaMedica\TRABALHOFINAL\Respiratory_Sound_Database\Respiratory_Sound_Database\patient_diagnosis.csv');
+T = readtable('C:\Users\f8\Desktop\EngenhariaMedica\TRABALHOFINAL\Respiratory_Sound_Database\Respiratory_Sound_Database\patient_diagnosis.csv');
 w = waitbar(0,'Relacionando classes');
 classes = table;
 for j = 1:size(S)
@@ -54,3 +55,26 @@ for j = 1:size(S)
 end
 delete(w);
 clear id j m pathname S T w
+            
+
+% % EXTRAÇÃO DE CARACTERÍSTICAS PRINCIPAIS
+media = zeros(size(ciclos));
+variancia = zeros(size(ciclos));
+mobilidade = zeros(size(ciclos));
+complex_estatistica = zeros(size(ciclos));
+freq_central = zeros(size(ciclos));
+largura_banda = zeros(size(ciclos));
+
+w = waitbar(0,'Extraindo características');
+for i=1:size(ciclos,1)
+    waitbar(i/size(ciclos,1));
+    for j=1:size(ciclos,2)
+        if isempty(ciclos{i,j})
+            break;
+        else
+            [media(i,j),variancia(i,j),mobilidade(i,j),complex_estatistica(i,j),freq_central(i,j),largura_banda(i,j)] = PRINCIPAIS_CARACTERISTICAS(ciclos{i,j}',fs(i));
+        end
+    end
+end
+delete(w);
+clear i j w
