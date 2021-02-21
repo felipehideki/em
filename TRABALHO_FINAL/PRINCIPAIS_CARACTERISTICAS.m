524c74ba7c8b574bb54746d88fd277a872f197f2
felipehideki <feehide@gmail.com>
2021-02-21 01:32:20 -0300

    Update PRINCIPAIS_CARACTERISTICAS.m

--- TRABALHO_FINAL/PRINCIPAIS_CARACTERISTICAS.m
+++ TRABALHO_FINAL/PRINCIPAIS_CARACTERISTICAS.m
@@ -14,13 +14,13 @@ function [media,variancia,mobilidade,complex_estatistica,freq_central,largura_ba
   freq_margem = zeros(qtd_trechos,1);
   
   for (trecho=1:qtd_trechos)
-    %%  MÉDIA
+    % %  MÉDIA
     media(trecho) = mean(SINAL(trecho,:));
     
-    %%  VARIÂNCIA
+    % %  VARIÂNCIA
     variancia(trecho) = var(SINAL(trecho,:));
     
-    %%  MOBILIDADE ESTATÍSTICA
+    % %  MOBILIDADE ESTATÍSTICA
     media_dif1 = 0;
     var_dif1 = 0;
     for (i=1:tamanho_sinal-1)
@@ -30,7 +30,7 @@ function [media,variancia,mobilidade,complex_estatistica,freq_central,largura_ba
     end
     mobilidade(trecho) = var_dif1/variancia(trecho);
       
-    %%  COMPLEXIDADE ESTATÍSTICA
+    % %  COMPLEXIDADE ESTATÍSTICA
     media_dif2 = 0;
     var_dif2 = 0;
     for (i=1:tamanho_sinal-2)
@@ -40,12 +40,12 @@ function [media,variancia,mobilidade,complex_estatistica,freq_central,largura_ba
     end
     complex_estatistica(trecho) = sqrt((var_dif2/var_dif1)-(var_dif1/variancia(trecho)));
 
-    %% FREQUÊNCIA CENTRAL
+    % % FREQUÊNCIA CENTRAL
     sigfft_shifted = fftshift(fft(SINAL(trecho,:)));
     potencia(trecho,:) = (abs(sigfft_shifted).^2)/tamanho_sinal;
     soma_pot = sum(potencia(trecho,1:floor(1+tamanho_sinal/2)));
-    %%plot(fshift,potencia);
-    %%xlim([0 FREQ_AMOSTRAGEM/2]);
+    %plot(fshift,potencia);
+    %xlim([0 FREQ_AMOSTRAGEM/2]);
     freq_central(trecho) = 0;
     for (i=1:tamanho_sinal/2)
       freq_central(trecho) = freq_central(trecho) + (fshift(i)*(-1)*potencia(trecho,i)/soma_pot);  %fshift*(-1) pois comeÃ§a da parte negativa do deslocamento
@@ -54,52 +54,13 @@ function [media,variancia,mobilidade,complex_estatistica,freq_central,largura_ba
     %%  LARGURA DE BANDA
     ILB_prov = 0;
     fw = 0;
-%     delta1 = 0;
-%     delta2 = 0;
-%     teta1 = 0;
-%     teta2 = 0;
-%     alfa = 0;
-%     beta = 0;
-%     gama = 0;
     for (i=1:tamanho_sinal/2)
       ILB_prov = ILB_prov + (((fshift(i)*(-1))-freq_central(trecho))^2)*potencia(trecho,i);  %fshift*(-1) pois comeÃ§a da parte negativa do deslocamento
-%       
-%       %%  FREQUÊNCIA DE MARGEM
+      % % FREQUÊNCIA DE MARGEM
       if (fw<0.9)
         fw = fw + potencia(trecho,floor(tamanho_sinal/2)+1-i)/soma_pot;
         freq_margem(trecho) = fshift(floor(tamanho_sinal/2)+1-i)*(-1);
       end
-%     
-%       %%  POTÊNCIA ESPECTRAL NORMALIZADA EM BANDAS  
-%       %  d1(0.5~2.5Hz), d2(2.5~4 Hz), t1(4~6Hz), t2(6~8Hz), a(8~12Hz), b(12~20 Hz) e g(20~45Hz)
-%       if ((fshift(i)*(-1))>20 && (fshift(i)*(-1))<45)
-%         gama = gama + potencia(trecho,i);
-%       end
-%       if ((fshift(i)*(-1))>12 && (fshift(i)*(-1))<20)
-%         beta = beta + potencia(trecho,i);
-%       end
-%       if ((fshift(i)*(-1))>8 && (fshift(i)*(-1))<12)
-%         alfa = alfa + potencia(trecho,i);
-%       end
-%       if ((fshift(i)*(-1))>6 && (fshift(i)*(-1))<8)
-%         teta2 = teta2 + potencia(trecho,i);
-%       end
-%       if ((fshift(i)*(-1))>4 && (fshift(i)*(-1))<6)
-%         teta1 = teta1 + potencia(trecho,i);
-%       end
-%       if ((fshift(i)*(-1))>2.5 && (fshift(i)*(-1))<4)
-%         delta2 = delta2 + potencia(trecho,i);
-%       end
-%       if ((fshift(i)*(-1))>0.5 && (fshift(i)*(-1))<2.5)
-%         delta1 = delta1 + potencia(trecho,i);
-%       end
     end
-      largura_banda(trecho) = sqrt(ILB_prov/soma_pot);
-%     pot_espect_norm(trecho,1) = delta1/soma_pot;
-%     pot_espect_norm(trecho,2) = delta2/soma_pot;
-%     pot_espect_norm(trecho,3) = teta1/soma_pot;
-%     pot_espect_norm(trecho,4) = teta2/soma_pot;
-%     pot_espect_norm(trecho,5) = alfa/soma_pot;
-%     pot_espect_norm(trecho,6) = beta/soma_pot;
-%     pot_espect_norm(trecho,7) = gama/soma_pot;
+    largura_banda(trecho) = sqrt(ILB_prov/soma_pot);
   end
