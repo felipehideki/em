d77bbb2ac11afb3e105791e0b9065ff71d3bbdd6
felipehideki <feehide@gmail.com>
2021-02-28 21:07:56 -0300

    Update CLASSIFICACAO_GRUPO1.m

--- TRABALHO_FINAL/CLASSIFICACAO_GRUPO1.m
+++ TRABALHO_FINAL/CLASSIFICACAO_GRUPO1.m
@@ -1,66 +1,82 @@
 % % TRABALHO FINAL DE ENGENHARIA MÉDICA 2S/2020
-% % GRUPO 1
+% % GRUPO 1: FELIPE HIDEKI HATANO & PEDRO ROCHA FERRAZ SANTOS
 
 % % CLASSIFICAÇÃO DE INDIVÍDUOS SAUDÁVEIS/DOENTES DE UM BANCO DE DADOS DE
 % % AUSCULTAÇÕES PULMONARES
 
 % % ---------------------------------------------------------------------
 % % SCRIPT PARA CLASSIFICAÇÃO
-% %     Utiliza os dados contidos no arquivo "DADOS_GRUPO1.mat", criado
-% % com os resultados do script "EXTRACAODADOS_GRUPO1.m".
+% %     Utiliza os dados contidos no arquivo "DADOS_GRUPO1.mat".
 % % ---------------------------------------------------------------------
+% % Observação: Os dados originais foram coletados do repositório
+% % <https://www.kaggle.com/vbookshelf/respiratory-sound-database>.
+% % Porém, por conta do grande volume de dados e o excessivo tempo de
+% % processamento para extração das características espectrais, optou-se
+% % por enviar os dados já processados para economia de recursos. 
+% % Entretanto, se necessário, os dados originais podem ser processados 
+% % pelo algorimo presente no arquivo "EXTRACAODADOS_GRUPO1.m".
 
+clear all;
+close all;
 load('DADOS_GRUPO1.mat');
 rng('default');
 
+% % ---------------------------------------------------------------------
+% %  CLASSIFICAÇÃO AGRUPAMENTO A: "SAUDÁVEIS x TODOS OS OUTROS"
+
+classes = numeracao;
+classes(classes~=1) = 2;
+xClasse{1} = dados(classes==1,:);
+xClasse{2} = dados(classes==2,:);
+
+% % PERCEPTRON POCKET PARA VERIFICAR SEPARABILIDADE LINEAR
+% [~,~,erroPocketAteste] = POCKET(xClasse{1},xClasse{2},10000,0.1);
+% 0.05 de erro, mas >95% dos dados são de uma única classe...
+% De qualquer modo, não é separável linearmente, o algoritmo não converge.
+
 % % REMOÇÃO DE OUTLIERS
 outliers = [];
-for i=1:size(dados,2)
-    [~,indexes{i}] = semana4_remocaooutliers(dados(:,i));
-    close all;
-    outliers = [outliers;indexes{i}];
+for j=1:size(xClasse,2)
+    for i=1:size(dados,2)
+        [~,indexes{i,j}] = semana4_remocaooutliers(xClasse{j}(:,i),3,0);
+        outliers = [outliers;indexes{i,j}];
+    end
 end
 outliers = unique(outliers);
-diagnostico = numeracao;
-diagnostico(outliers) = [];
-dados(outliers,:) = [];
+classes(outliers) = [];
+dadosA = dados;
+dadosA(outliers,:) = [];
 crackle_wheeze(outliers,:) = []; % Para caso seja utilizado posteriormente
+xClasse{1} = dadosA(classes==1,:);
+xClasse{2} = dadosA(classes==2,:);
 clear i indexes outliers
 
+% HISTOGRAMA
+figure;
+histogram(classes,2);
+[n,x] = hist(classes,2);
+barstrings = num2str(n');
+text(x,n,barstrings,'horizontalalignment','center','verticalalignment','bottom');
+title('Distribuição dos padrões (agrupamento A)');
+xlabel('classe (1 = Positiva, 2 = Negativa)');
+ylabel('padrões');
+ax = gca;
+ax.XTick = [1 2];
+clear n x ax
 
-%%  CLASSIFICAÇÃO AGRUPAMENTO A: "SAUDÁVEIS x TODOS OS OUTROS"
-
-dx = diagnostico;
-dx(dx~=1) = 2;
-
-% % HISTOGRAMA
-% histogram(dx,2);
-% [n,x] = hist(dx,2);
-% barstrings = num2str(n');
-% text(x,n,barstrings,'horizontalalignment','center','verticalalignment','bottom');
-% title('Distribuição dos padrões (1 = SAUDÁVEIS, 2 = NÃO-SAUDÁVEIS)');
-% xlabel('classe');
-% ylabel('padrões');
-% ax = gca;
-% ax.XTick = [1 2];
-% clear n x ax
+% % PERCEPTRON POCKET PARA VERIFICAR SEPARABILIDADE LINEAR
+% [~,~,erroPocketAteste] = POCKET(xClasse{1},xClasse{2},10000,0.1);
+% Não é separável linearmente, o algoritmo não converge.
 
 % % NORMALIZAÇÃO DOS DADOS
-xClasse1 = dados(dx==1,:);
-xClasse2 = dados(dx==2,:);
-todas = [xClasse1;xClasse2];
+todas = [xClasse{1};xClasse{2}];
 padroes_norm = zeros(size(todas,1),size(todas,2));
-for i=1:size(xClasse1,2)
+for i=1:size(todas,2)
     padroes_norm(:,i) = semana4_normalizacao(todas(:,i)');
 end
 clear todas i
-saudaveis = padroes_norm(1:size(xClasse1,1),:);
-doentes = padroes_norm(size(xClasse1,1)+1:end,:);
-
-% % PERCEPTRON POCKET PARA VERIFICAR SEPARABILIDADE LINEAR
-[~,~,erroPocketA] = POCKET(saudaveis,doentes,10000,0.1);
-% 0.05 de erro, mas >95% dos dados são da classe não-saudável...
-% Não é separável linearmente, o algoritmo não converge.
+saudaveis = padroes_norm(1:size(xClasse{1},1),:);
+doentes = padroes_norm(size(xClasse{1},1)+1:end,:);
 
 % % PCA
 erroPCA = 0;
@@ -72,34 +88,34 @@ end
 [~,~,matrizPCA] = KL(padroes_norm',i+1);
 clear i erroPCA;
 matrizPCA = matrizPCA';
-% Espaço reduzido para 9 dimensões após PCA
-
+% Espaço reduzido para 10 dimensões após PCA
 
 % % CLASSIFICAÇÃO COM PERCEPTRON
-saudaveisPCA = matrizPCA(1:size(xClasse1,1),:);
-doentesPCA = matrizPCA(size(xClasse1,1)+1:end,:);
+saudaveisPCA = matrizPCA(1:size(xClasse{1},1),:);
+doentesPCA = matrizPCA(size(xClasse{1},1)+1:end,:);
 Y = [ones(size(saudaveisPCA,1),1);-ones(size(doentesPCA,1),1)];
 [~,~,erroPocketA,indicesYe] = POCKET(saudaveisPCA,doentesPCA,10000,0.1);
 
 % Histograma do resultado da classificação Perceptron
+figure;
 classificacaoP = Y';
 classificacaoP(indicesYe) = -classificacaoP(indicesYe);
-H = [sum(classificacaoP+Y'<0) sum(classificacaoP+Y'==0) sum(classificacaoP+Y'>0)];
+H = [sum(classificacaoP+Y'>0) sum(classificacaoP+Y'==0) sum(classificacaoP+Y'<0)];
 bar(H);
 text(1:length(H),H,num2str(H'),'vert','bottom','horiz','center'); 
 box off
 title({['Resultado Perceptron (agrupamento A)'];['Acurácia: ', num2str(100*(1-erroPocketA),'%.2f'),'%']});
-xlabel('1 = VN, 2 = FN+FP, 3 = VP');
+xlabel('1 = VP, 2 = FN+FP, 3 = VN');
 ylabel('padroes');
 
-% % Histograma de percentual dos erros Perceptron
-% FN_POCKETA = sum(indicesYe<=size(saudaveisPCA,1))/size(saudaveisPCA,1);
-% FP_POCKETA = sum(indicesYe>size(saudaveisPCA,1))/size(doentesPCA,1);
-% bar(100*[FN_POCKETA FP_POCKETA]);
-% title('Percentual de erros Perceptron (agrupamento A)');
-% xlabel('1 = FN, 2 = FP');
-% ylabel('%');
-
+% Histograma de percentual dos erros Perceptron
+figure;
+FN_POCKETA = sum(indicesYe<=size(saudaveisPCA,1))/size(saudaveisPCA,1);
+FP_POCKETA = sum(indicesYe>size(saudaveisPCA,1))/size(doentesPCA,1);
+bar(100*[FN_POCKETA FP_POCKETA]);
+title('Percentual de erros Perceptron (agrupamento A)');
+xlabel('1 = FN, 2 = FP');
+ylabel('%');
 
 % % CLASSIFICAÇÃO COM SVM LINEAR
 [alpha, w0] = semana9_SVM(matrizPCA,Y,'linear');
@@ -110,27 +126,27 @@ coefs = alpha(indices_svms).*Y(indices_svms);
 
 % Histograma do resultado da classificação SVM
 figure;
-H = [sum(classificacao+Y'<0) sum(classificacao+Y'==0) sum(classificacao+Y'>0)];
+H = [sum(classificacao+Y'>0) sum(classificacao+Y'==0) sum(classificacao+Y'<0) ];
 bar(H);
 text(1:length(H),H,num2str(H'),'vert','bottom','horiz','center'); 
 box off
 title({['Resultado SVM (agrupamento A)'];['Acurácia: ', num2str(100-erroSVMA,'%.2f'),'%']});
-xlabel('1 = VN, 2 = FN+FP, 3 = VP');
+xlabel('1 = VP, 2 = FN+FP, 3 = VN');
 ylabel('padrões');
 
 % % Histograma de percentual dos erros SVM
-% FN_SVMA = (sum(Y'==1)-sum(classificacao+Y'==2))/sum(Y'==1);
-% FP_SVMA = (sum(Y'==-1)-sum(classificacao+Y'==-2))/sum(Y'==-1);
-% bar(100*[FN_SVMA FP_SVMA]);
-% title('Percentual de erros SVM (A)');
-% xlabel('1 = FN, 2 = FP');
-% ylabel('%');
+figure;
+FN_SVMA = (sum(Y'==1)-sum(classificacao+Y'==2))/sum(Y'==1);
+FP_SVMA = (sum(Y'==-1)-sum(classificacao+Y'==-2))/sum(Y'==-1);
+bar(100*[FN_SVMA FP_SVMA]);
+title('Percentual de erros SVM (agrupamento A)');
+xlabel('1 = FN, 2 = FP');
+ylabel('%');
 
 % AUC da característica 1
 % aucA1 = ROC(matrizPCA(:,1),Y);
 
 % % AVALIAÇÃO DE POSSÍVEIS SUBAJUSTES E SOBREAJUSTES
-
 part = cvpartition(size(matrizPCA,1),'KFold',10);
 Q = [matrizPCA ones(size(matrizPCA,1),1)];
 
@@ -142,9 +158,9 @@ for i=1:10
     waitbar(i/10);
     fold = matrizPCA(training(part,i),:);% dados de treino
     Z = Y(training(part,i));
-    xClasse1 = fold(Z>0,:);
-    xClasse2 = fold(Z<0,:);
-    [wp,~,erroPtrainingA(i)] = POCKET(xClasse1,xClasse2,10000,0.1);
+    xClasse{1} = fold(Z>0,:);
+    xClasse{2} = fold(Z<0,:);
+    [wp,~,erroPtrainingA(i)] = POCKET(xClasse{1},xClasse{2},10000,0.1);
     pk = Q*wp;
     pk(pk(test(part,i))<0) = 1;
     pk(pk(test(part,i))>0) = -1;
@@ -152,7 +168,6 @@ for i=1:10
 end
 erroPtrainingA = 100*erroPtrainingA;
 delete(wb); clear wb;
-
 
 % SVM
 erroSVMtrainingA = zeros(10,1);
@@ -175,43 +190,71 @@ end
 delete(wb);
 
 
-%% CLASSIFICAÇÃO AGRUPAMENTO B: "SAUDÁVEIS x TODOS (5% DPOC)"
+% % ---------------------------------------------------------------------
+% % CLASSIFICAÇÃO AGRUPAMENTO B: "SAUDÁVEIS x TODOS (5% DPOC)"
 
 % % PARTICIONANDO 5% DOS PADRÕES DE DPOC
-c = cvpartition(sum(diagnostico==3),'KFold',20);
+c = cvpartition(sum(numeracao==3),'KFold',20);
 idx = test(c,1);
-dpoc = dados(diagnostico==3,:);
+dpoc = dados(numeracao==3,:);
 dpoc = dpoc(idx,:);
 
-xClasse1 = dados(diagnostico==1,:);
-xClasse2 = dados(diagnostico~=1 & diagnostico~=3,:);
-xClasse2 = [xClasse2; dpoc];
-classes = [ones(size(xClasse1,1),1);2*ones(size(xClasse1,1),1)];
+clear classes xClasse
+xClasse{1} = dados(numeracao==1,:);
+xClasse{2} = dados(numeracao~=1 & numeracao~=3,:);
+xClasse{2} = [xClasse{2}; dpoc];
+classes = [ones(size(xClasse{1},1),1);2*ones(size(xClasse{2},1),1)];
 
-% % HISTOGRAMA
-% histogram(classes,2);
-% [n,x] = hist(classes,2);
-% barstrings = num2str(n');
-% text(x,n,barstrings,'horizontalalignment','center','verticalalignment','top');
-% title('Distribuição dos padrões (1 = SAUDÁVEIS, 2 = NÃO-SAUDÁVEIS)');
-% xlabel('classe');
-% ylabel('padrões');
-% ax = gca;
-% ax.XTick = [1 2];
-% clear n x ax
+% % PERCEPTRON POCKET PARA VERIFICAR SEPARABILIDADE LINEAR
+% [~,~,erroPocketBteste] = POCKET(xClasse{1},xClasse{2},10000,0.1);
+% Não é separável linearmente, o algoritmo não converge.
+
+% % REMOÇÃO DE OUTLIERS
+outliers = [];
+for j=1:size(xClasse,2)
+    for i=1:size(dados,2)
+        [~,indexes{i,j}] = semana4_remocaooutliers(xClasse{j}(:,i),3,0);
+        outliers = [outliers;indexes{i,j}];
+    end
+end
+outliers = unique(outliers);
+classes(outliers) = [];
+dadosB = dados;
+dadosB(outliers,:) = [];
+crackle_wheeze(outliers,:) = []; % Para caso seja utilizado posteriormente
+xClasse{1} = dadosB(classes==1,:);
+xClasse{2} = dadosB(classes==2,:);
+clear i indexes outliers
+
+% HISTOGRAMA
+figure;
+histogram(classes,2);
+[n,x] = hist(classes,2);
+barstrings = num2str(n');
+text(x,n,barstrings,'horizontalalignment','center','verticalalignment','bottom');
+title('Distribuição dos padrões (agrupamento B)');
+xlabel('classe (1 = Positiva, 2 = Negativa)');
+ylabel('padrões');
+ax = gca;
+ax.XTick = [1 2];
+clear n x ax
+
+% % PERCEPTRON POCKET PARA VERIFICAR SEPARABILIDADE LINEAR
+% [~,~,erroPocketBteste] = POCKET(xClasse{1},xClasse{2},10000,0.1);
+% Não é separável linearmente, o algoritmo não converge.
 
 % % NORMALIZAÇÃO DOS DADOS
-todas = [xClasse1;xClasse2];
+todas = [xClasse{1};xClasse{2}];
 padroes_norm = zeros(size(todas,1),size(todas,2));
-for i=1:size(xClasse1,2)
+for i=1:size(xClasse{1},2)
     padroes_norm(:,i) = semana4_normalizacao(todas(:,i)');
 end
 clear todas i
-saudaveis = padroes_norm(1:size(xClasse1,1),:);
-doentes = padroes_norm(size(xClasse1,1)+1:end,:);
+saudaveis = padroes_norm(1:size(xClasse{1},1),:);
+doentes = padroes_norm(size(xClasse{1},1)+1:end,:);
 
 % % PERCEPTRON POCKET PARA VERIFICAR SEPARABILIDADE LINEAR
-[~,~,erroPocketB] = POCKET(saudaveis,doentes,10000,0.1);
+% [~,~,erroPocketB] = POCKET(saudaveis,doentes,10000,0.1);
 % 0.05 de erro, mas >95% dos dados são da classe não-saudável...
 % Não é separável linearmente, o algoritmo não converge.
 
@@ -227,33 +270,32 @@ clear i erroPCA;
 matrizPCA = matrizPCA';
 % Espaço reduzido para 9 dimensões após PCA
 
-
 % % CLASSIFICAÇÃO COM PERCEPTRON
-saudaveisPCA = matrizPCA(1:size(xClasse1,1),:);
-doentesPCA = matrizPCA(size(xClasse1,1)+1:end,:);
+saudaveisPCA = matrizPCA(1:size(xClasse{1},1),:);
+doentesPCA = matrizPCA(size(xClasse{1},1)+1:end,:);
 Y = [ones(size(saudaveisPCA,1),1);-ones(size(doentesPCA,1),1)];
-[~,~,erroPocketA,indicesYe] = POCKET(saudaveisPCA,doentesPCA,10000,0.1);
+[~,~,erroPocketB,indicesYe] = POCKET(saudaveisPCA,doentesPCA,10000,0.1);
 
 % Histograma do resultado da classificação Perceptron
 figure;
 classificacaoP = Y';
 classificacaoP(indicesYe) = -classificacaoP(indicesYe);
-H = [sum(classificacaoP+Y'<0) sum(classificacaoP+Y'==0) sum(classificacaoP+Y'>0)];
+H = [sum(classificacaoP+Y'>0) sum(classificacaoP+Y'==0) sum(classificacaoP+Y'<0)];
 bar(H);
 text(1:length(H),H,num2str(H'),'vert','bottom','horiz','center'); 
 box off
 title({['Resultado Perceptron (agrupamento B)'];['Acurácia: ', num2str(100*(1-erroPocketB),'%.2f'),'%']});
-xlabel('1 = VN, 2 = FN+FP, 3 = VP');
+xlabel('1 = VP, 2 = FN+FP, 3 = VN');
 ylabel('padroes');
 
-% % Histograma de percentual dos erros Perceptron
-% FN_POCKETA = sum(indicesYe<=size(saudaveisPCA,1))/size(saudaveisPCA,1);
-% FP_POCKETA = sum(indicesYe>size(saudaveisPCA,1))/size(doentesPCA,1);
-% bar(100*[FN_POCKETA FP_POCKETA]);
-% title('Percentual de erros Perceptron (agrupamento A)');
-% xlabel('1 = FN, 2 = FP');
-% ylabel('%');
-
+% Histograma de percentual dos erros Perceptron
+figure;
+FN_POCKETA = sum(indicesYe<=size(saudaveisPCA,1))/size(saudaveisPCA,1);
+FP_POCKETA = sum(indicesYe>size(saudaveisPCA,1))/size(doentesPCA,1);
+bar(100*[FN_POCKETA FP_POCKETA]);
+title('Percentual de erros Perceptron (agrupamento B)');
+xlabel('1 = FN, 2 = FP');
+ylabel('%');
 
 % % CLASSIFICAÇÃO COM SVM LINEAR
 [alpha, w0] = semana9_SVM(matrizPCA,Y,'linear');
@@ -264,21 +306,22 @@ coefs = alpha(indices_svms).*Y(indices_svms);
 
 % Histograma do resultado da classificação SVM
 figure;
-H = [sum(classificacao+Y'<0) sum(classificacao+Y'==0) sum(classificacao+Y'>0)];
+H = [sum(classificacao+Y'>0) sum(classificacao+Y'==0) sum(classificacao+Y'<0) ];
 bar(H);
 text(1:length(H),H,num2str(H'),'vert','bottom','horiz','center'); 
 box off
 title({['Resultado SVM (agrupamento B)'];['Acurácia: ', num2str(100-erroSVMB,'%.2f'),'%']});
-xlabel('1 = VN, 2 = FN+FP, 3 = VP');
+xlabel('1 = VP, 2 = FN+FP, 3 = VN');
 ylabel('padrões');
 
-% % Histograma de percentual dos erros SVM
-% FN_SVMA = (sum(Y'==1)-sum(classificacao+Y'==2))/sum(Y'==1);
-% FP_SVMA = (sum(Y'==-1)-sum(classificacao+Y'==-2))/sum(Y'==-1);
-% bar(100*[FN_SVMA FP_SVMA]);
-% title('Percentual de erros SVM (A)');
-% xlabel('1 = FN, 2 = FP');
-% ylabel('%');
+% Histograma de percentual dos erros SVM
+figure;
+FN_SVMA = (sum(Y'==1)-sum(classificacao+Y'==2))/sum(Y'==1);
+FP_SVMA = (sum(Y'==-1)-sum(classificacao+Y'==-2))/sum(Y'==-1);
+bar(100*[FN_SVMA FP_SVMA]);
+title('Percentual de erros SVM (agrupamento B)');
+xlabel('1 = FN, 2 = FP');
+ylabel('%');
 
 % AUC da característica 1
 % aucB1 = ROC(matrizPCA,Y);
@@ -297,9 +340,9 @@ for i=1:10
     waitbar(i/10);
     fold = matrizPCA(training(part,i),:);% dados de treino
     Z = Y(training(part,i));
-    xClasse1 = fold(Z>0,:);
-    xClasse2 = fold(Z<0,:);
-    [wp,~,erroPtrainingB(i)] = POCKET(xClasse1,xClasse2,10000,0.1);
+    xClasse{1} = fold(Z>0,:);
+    xClasse{2} = fold(Z<0,:);
+    [wp,~,erroPtrainingB(i)] = POCKET(xClasse{1},xClasse{2},10000,0.1);
     pk = Q*wp;
     pk(pk(test(part,i))<0) = 1;
     pk(pk(test(part,i))>0) = -1;
